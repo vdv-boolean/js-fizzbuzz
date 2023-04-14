4d9ac43752cfb45dcd7cd9d88b7f69bfae249bba
vincenzodivita <divitavincenzo@icloud.com>
2023-04-14 15:21:49 +0200

    Remove console.log()

--- assets/script.js
+++ assets/script.js
@@ -6,19 +6,19 @@ const container = document.querySelector('.container');
 
 for (let i = 1; i <= numOfSquare; i++) {
     if (i % 15 == 0) {
-        console.log('FizzBuzz');
+        // console.log('FizzBuzz');
         container.innerHTML = container.innerHTML + `<div class="square fizzbuzz">FizzBuzz</div>`;
     }
     else if (i % 5 == 0) {
-        console.log('Buzz');
+        // console.log('Buzz');
         container.innerHTML = container.innerHTML + `<div class="square buzz">Buzz</div>`;
     }
     else if (i % 3 == 0) {
-        console.log('Fizz');
+        // console.log('Fizz');
         container.innerHTML = container.innerHTML + `<div class="square fizz">Fizz</div>`;      
     }
     else {
-        console.log(i);
+        // console.log(i);
         container.innerHTML = container.innerHTML + `<div class="square">${i}</div>`;
     }   
 }
