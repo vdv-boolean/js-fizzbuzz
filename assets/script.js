1a8b627addd32a1abf7e4d09ece400aea113a6de
vincenzodivita <divitavincenzo@icloud.com>
2023-04-14 15:17:42 +0200

    Add last case (when number is multiple of 15)

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,17 @@ const container = document.querySelector('.container');
 // For loop
 
 for (let i = 1; i <= numOfSquare; i++) {
-    if (i % 3 == 0) {
-        console.log('Fizz');
-        container.innerHTML = container.innerHTML + `<div class="square fizz">Fizz</div>`;
+    if (i % 15 == 0) {
+        console.log('FizzBuzz');
+        container.innerHTML = container.innerHTML + `<div class="square fizzbuzz">FizzBuzz</div>`;
     }
     else if (i % 5 == 0) {
         console.log('Buzz');
         container.innerHTML = container.innerHTML + `<div class="square buzz">Buzz</div>`;
+    }
+    else if (i % 3 == 0) {
+        console.log('Fizz');
+        container.innerHTML = container.innerHTML + `<div class="square fizz">Fizz</div>`;      
     }
     else {
         console.log(i);
